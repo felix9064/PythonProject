4ff22e8b533d8b4f7c663712459f4c0aad8ac0a2
felix9064 <zhang1zhi7hua5@gmail.com>
2015-12-24 00:02:41 +0800

    切片

---
+++ Simples/do_slice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 切片
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print(L[0:3])
+
+newList = list(range(100))
+print(newList[:20:2])
+print(newList[::5])
+
