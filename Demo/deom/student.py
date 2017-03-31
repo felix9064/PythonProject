b26b8f7d091026b4b168e1da7c3b9830a989b1a2
felix9064 <zhang1zhi7hua5@gmail.com>
2017-03-31 22:00:33 +0800

    人生苦短，我用Python

--- Demo/deom/student.py
+++ Demo/deom/student.py
@@ -1,3 +1,4 @@
+
 class Student(object):
     def __init__(self, name, score):
         self.name = name
@@ -6,5 +7,14 @@ class Student(object):
     def print_score(self):
         print('%s: %s' % (self.name, self.score))
 
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 80:
+            return 'B'
+        else:
+            return 'C'
+
 bart = Student('zhangsan', 88)
 bart.print_score()
+print(bart.get_grade())
