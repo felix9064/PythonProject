d8cc419d814a9ef00fe017170086874891b4dc8f
felix <zhang1zhi7hua5@gmail.com>
2017-09-01 08:36:26 +0800

    人生苦短，我用Python

---
+++ Demo/demo/the_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 字符串相关的一些操作和函数
+
+# 字符串的title()方法使单词首字母大写
+name = "felix chang"
+print(name.title())
+
+# 删除字符串右边的空白字符使用 rstrip()函数
+favorite_language = " java python "
+print(favorite_language.rstrip())
+
+#
