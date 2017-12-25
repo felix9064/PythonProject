f1d599121286300a3a882a641acaf4e356ece548
felix <zhang1zhi7hua5@gmail.com>
2017-12-25 17:36:50 +0800

    Python编程示例

--- Demo/runoob/fibonacci.py
+++ Demo/runoob/fibonacci.py
@@ -7,7 +7,20 @@
 
 # 关键字end可以用于将结果输出到同一行，或者在输出的末尾添加不同的字符
 
-a, b = 0, 1
-while b < 1000:
-    print(b, end=',')
-    a, b = b, a + b
+
+def fib1(index):
+    a, b = 0, 1
+    while b < index:
+        print(b, end=',')
+        a, b = b, a + b
+
+
+def fib2(index):
+    n, a, b = 0, 0, 1
+    while n < index:
+        print(b, end=" ")
+        a, b = b, a + b
+        n = n + 1
+    return "done"
+
+fib2(10)
