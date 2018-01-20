b8103a0bb7a98325c9db6874088afccfbb7c35bb
felix <zhang1zhi7hua5@gmail.com>
2018-01-20 19:44:16 +0800

    Python 斐波那契数列修改

--- Demo/runoob/fibonacci.py
+++ Demo/runoob/fibonacci.py
@@ -13,6 +13,7 @@ def fib1(index):
     while b < index:
         print(b, end=',')
         a, b = b, a + b
+    print()
 
 
 def fib2(index):
@@ -21,6 +22,25 @@ def fib2(index):
         print(b, end=" ")
         a, b = b, a + b
         n = n + 1
+    print()
     return "done"
 
+
+def fib3(index):
+    lst = [1, 1]
+    for i in range(index - 2):
+        lst.append(lst[-1] + lst[-2])
+    print(list(lst))
+
+
+def fib4(index):
+    a, b, lst = 0, 1, []
+    for i in range(index):
+        lst.append(a)
+        a, b = b, a + b
+    return lst
+
+fib1(60)
 fib2(10)
+fib3(10)
+print(list(fib4(10)))
