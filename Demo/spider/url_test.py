4fdfefd50ec5ebd1eabe31244550205c8cb601f8
felix <zhang1zhi7hua5@gmail.com>
2018-03-04 13:22:50 +0800

    python代码

--- Demo/spider/url_test.py
+++ Demo/spider/url_test.py
@@ -4,7 +4,8 @@
 import urllib.request
 import http.cookiejar
 
-url = "https://www.baidu.com"
+url = "http://img.mmjpg.com/2018/1271/1io7.jpg"
+"""
 print("第一种解析方法")
 response1 = urllib.request.urlopen(url)
 print(response1.getcode())
@@ -15,12 +16,18 @@ request.add_header("user-agent", "Mozilla/5.0")
 response2 = urllib.request.urlopen(request)
 print(response2.getcode())
 print(len(response2.read()))
-
+"""
 cookie = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
 urllib.request.install_opener(opener)
 response3 = urllib.request.urlopen(url)
+
+for k, v in response3.getheaders():
+    data = response3.read()
+    print('%s: %s' % (k, v))
+    # print('Data:', data.decode('utf-8'))
+"""
 print(response3.getcode())
 print(cookie)
 print(response3.read())
-
+"""
