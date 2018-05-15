e03f2152af316b70ca1ee66dc2b0f179c8906261
felix <zhang1zhi7hua5@gmail.com>
2018-05-15 13:35:09 +0800

    定义__format__特殊方法，让对象可以支持自定义的格式化

--- Demo/cookbook/ch08/p02_custom_format.py
+++ Demo/cookbook/ch08/p02_custom_format.py
@@ -5,3 +5,31 @@
 为了自定义字符串的格式化，我们需要在类上面定义 __format__() 方法
 """
 
+
+_formats = {
+    'ymd': '{d.year}-{d.month}-{d.day}',
+    'mdy': '{d.month}/{d.day}/{d.year}',
+    'dmy': '{d.day}/{d.month}/{d.year}'
+}
+
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __format__(self, format_spec):
+        if format_spec == '':
+            format_spec = 'ymd'
+        fmt = _formats[format_spec]
+        return fmt.format(d=self)
+
+
+if __name__ == '__main__':
+    d = Date(2018, 5, 15)
+    print(format(d))
+    print(format(d, 'mdy'))
+
+    print('The date is {:ymd}'.format(d))
+    print('The date is {:mdy}'.format(d))
