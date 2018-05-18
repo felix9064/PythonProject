220653aa883de8b92dd80a5e36a1495fe7b26262
felix <zhang1zhi7hua5@gmail.com>
2018-05-18 15:29:11 +0800

    流程的Python 书中示例

---
+++ Demo/fluent_python/demo_bisect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 流畅的Python 第二章示例程序2-17
+# bisect模块中的函数利用二分查找算法来在有序序列中查找或插入元素
+
+import bisect
+import os
+
+HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+
+
+if __name__ == "__main__":
+    pass
